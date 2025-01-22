b219532f8451a33e18858000581e7b1d77d3ad71
unknown <calebepfigueira@gmail.com>
2025-01-22 10:59:41 -0300

    Ate´aula 102

---
+++ Projetos_3/01_def_funcçao/script.js
@@ -0,0 +1,22 @@
+function imprimirNoconsole() {
+    console.log("Hello World");
+
+imprimirNoconsole();
+}
+
+function imprimirUmnumero(num) {
+    console.log("O numero é " + num);
+}
+
+imprimirUmnumero(2);
+imprimirUmnumero(7);
+imprimirUmnumero(2100);
+imprimirUmnumero(10);
+imprimirUmnumero(5);
+
+const numeroAleatório = function() {
+    console.log(Math.random());
+}
+
+numeroAleatório();
+numeroAleatório();
